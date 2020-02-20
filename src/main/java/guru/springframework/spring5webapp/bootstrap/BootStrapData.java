6d99d77c6ba150c99f2389bd47851420a7176ca5
mpacala00 <mpacala00@gmail.com>
2020-02-20 17:40:22 +0100

    Mapping a publisher to books

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -28,31 +28,53 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        System.out.println("Started in Bootstrap");
+
+        Publisher publisher = new Publisher();
+        publisher.setName("Helion SA");
+        publisher.setAdressLine("Kościuszki 1c");
+        publisher.setCity("Gliwice");
+
+        publisherRepository.save(publisher);
+        System.out.println("Number of publishers: "+publisherRepository.count());
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
-        //Dzięki temu zapiszą się do H2 Database
+
+        //"Helion SA", "Kościuszki 1c", "Gliwice",
+        //                "Sląskie", "44-100");
+
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
+
+        //To BARDZO BARDZO BARDZO ważne, żeby publisherRepo było na samym końcu, po updatcie bookRepo
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod", "Johnson"); //Founder of Spring
         Book noEJB = new Book("J2EE Development without EJB", "827132");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
 
-        System.out.println("Started in Bootstrap");
         System.out.println("Number of books: "+bookRepository.count());
+        System.out.println("Number of books by publisher: "+publisher.getBooks().size());
 
-        Publisher helion = new Publisher("Helion SA", "Kościuszki 1c", "Gliwice",
-                "Sląskie", "44-100");
-        publisherRepository.save(helion);
+
+        /*
         System.out.println("Publisher test");
         System.out.println("Number of publishers: "+publisherRepository.count());
+        */
 
     }
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -1,9 +1,8 @@
 package guru.springframework.spring5webapp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -18,6 +17,12 @@ public class Publisher {
     private String state;
     private String zip;
 
+    //Najlepiej to od razu inicjalizować, na razie jest to najprostszy sposób na te Sety
+
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
+
     public Publisher() {
     }
 
@@ -27,6 +32,14 @@ public class Publisher {
         this.city = city;
         this.state = state;
         this.zip = zip;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     public String getName() {
